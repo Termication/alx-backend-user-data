d2e220b62c4b6e7291eb0c4c6e52f601e27c4ef9
Innocent <terminalkarabo@gmail.com>
2024-11-06 19:21:16 +0000

    a function called filter_datum that returns the log message obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,20 @@
+#!/usr/bin/env python3
+"""
+filtered_logger
+"""
+
 import re
 
-def filter_datum(fields, redaction, message, separator):
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """
     Obfuscate specific fields in a log message.
     """
-    pattern = r'(' + '|'.join([f"{field}=[^;]*" for field in fields]) + ')'
-    return re.sub(pattern, lambda match: match.group(0).split('=')[0] + '=' + redaction, message)
+    for field in fields:
+        message = re.sub(
+            field + "=.*?" + separator, field + "=" +
+            redaction + separator, message
+        )
+    return message
